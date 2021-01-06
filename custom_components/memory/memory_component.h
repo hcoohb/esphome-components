042f86f98bacbf7ab1dfe471028f2decd518fec9
hcoohb <hcoohb@gmail.com>
2021-01-06 16:42:39 +1000

    Memory component

---
+++ custom_components/memory/memory_component.h
@@ -0,0 +1,17 @@
+// https://github.com/ayufan/esphome-components
+
+#pragma once
+
+#include "esphome/core/component.h"
+
+namespace esphome {
+namespace debug {
+
+class MemoryComponent : public PollingComponent {
+ public:
+  void update() override;
+  float get_setup_priority() const override;
+};
+
+}  // namespace debug
+}  // namespace esphome
