12df33aa51b08832bfa12ad155a6859f7e81aa59
hcoohb <hcoohb@gmail.com>
2021-11-09 14:05:35 +1000

    fix memory component for esphome 2021.10

--- custom_components/memory/memory_sensor.cpp
+++ custom_components/memory/memory_sensor.cpp
@@ -4,13 +4,27 @@
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
 
+#ifdef USE_ARDUINO
+#include <Esp.h>
+#endif
+
+#ifdef USE_ESP_IDF
+#include <esp_heap_caps.h>
+#include <esp_system.h>
+#endif
+
 namespace esphome {
 namespace memory {
 
 static const char *TAG = "memory.sensor";
 
 void MemorySensor::update() {
-  uint32_t free_heap = ESP.getFreeHeap();
+  #ifdef USE_ARDUINO
+    uint32_t free_heap = ESP.getFreeHeap();  // NOLINT(readability-static-accessed-through-instance)
+  #elif defined(USE_ESP_IDF)
+    uint32_t free_heap = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+  #endif
+  // uint32_t free_heap = ESP.getFreeHeap();
   // ESP_LOGD(TAG, "Free Heap Size: %u bytes", free_heap);
   this->publish_state(free_heap);
 }
