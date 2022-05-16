9cc2f9387a82c072f94f3909090c693009283751
hcoohb <hcoohb@gmail.com>
2022-05-16 17:41:21 +1000

    Codegen tidyup

--- components/lm75a/sensor.py
+++ components/lm75a/sensor.py
@@ -2,20 +2,28 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import core
 from esphome.components import i2c, sensor
-from esphome.const import CONF_ID, UNIT_CELSIUS, ICON_THERMOMETER
+from esphome.const import UNIT_CELSIUS, ICON_THERMOMETER, DEVICE_CLASS_TEMPERATURE, STATE_CLASS_MEASUREMENT
 
 DEPENDENCIES = ['i2c']
 
 lm75a_ns = cg.esphome_ns.namespace('lm75a')
-
 LM75AComponent = lm75a_ns.class_('LM75AComponent', sensor.Sensor, cg.PollingComponent, i2c.I2CDevice)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_CELSIUS, ICON_THERMOMETER, 1).extend({
-    cv.GenerateID(): cv.declare_id(LM75AComponent),}).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x48))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        LM75AComponent,
+        unit_of_measurement=UNIT_CELSIUS,
+        device_class=DEVICE_CLASS_TEMPERATURE,
+        icon=ICON_THERMOMETER,
+        accuracy_decimals=1,
+        state_class=STATE_CLASS_MEASUREMENT
+    )
+    .extend(cv.polling_component_schema('60s'))
+    .extend(i2c.i2c_device_schema(0x48))
+)
 
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
-    yield i2c.register_i2c_device(var, config)
+async def to_code(config):
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
--- components/memory/sensor.py
+++ components/memory/sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-from esphome.const import CONF_ID
+from esphome.const import DEVICE_CLASS_EMPTY, STATE_CLASS_MEASUREMENT
 
 UNIT_BYTE = 'B'
 ICON_MEMORY = 'mdi:memory'
@@ -9,12 +9,19 @@ ICON_MEMORY = 'mdi:memory'
 memory_ns = cg.esphome_ns.namespace('memory')
 MemorySensor = memory_ns.class_('MemorySensor', sensor.Sensor, cg.PollingComponent)
 
-CONFIG_SCHEMA = sensor.sensor_schema(UNIT_BYTE, ICON_MEMORY, 0).extend({
-    cv.GenerateID(): cv.declare_id(MemorySensor),
-}).extend(cv.polling_component_schema('60s'))
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        MemorySensor,
+        unit_of_measurement=UNIT_BYTE,
+        device_class=DEVICE_CLASS_EMPTY,
+        icon=ICON_MEMORY,
+        accuracy_decimals=0,
+        state_class=STATE_CLASS_MEASUREMENT
+    )
+    .extend(cv.polling_component_schema('60s'))
+)
 
 
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield sensor.register_sensor(var, config)
+async def to_code(config):
+    var = await sensor.new_sensor(config)
+    await cg.register_component(var, config)
